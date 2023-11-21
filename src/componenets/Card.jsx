ab2307492ece1e92767dd98ab42f2c02971f9072
nafisreza <bigganinafis@gmail.com>
2023-11-21 23:44:27 +0600

    💫 Card component shadow fixed

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card() {
   return (
-    <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-full bg-white border border-gray-400 rounded-lg shadow-2xl dark:bg-gray-800 dark:border-gray-700">
       <a href="/">
         <img
           className="p-8 rounded-t-lg"
