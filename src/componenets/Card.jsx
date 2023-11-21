d36f846492609629f0e0fd834de89b9949ad5362
nafisreza <bigganinafis@gmail.com>
2023-11-21 19:48:49 +0600

    ✨ Card component text made better

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -13,7 +13,7 @@ export default function Card() {
       <div className="px-5 pb-5">
         <a href="/">
           <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            Monica Bellucci
+            Malèna (2000)
           </h5>
         </a>
         <div className="flex items-center mt-2.5 mb-5">
@@ -66,13 +66,13 @@ export default function Card() {
             4.0
           </span>
         </div>
-        <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            Malena (2000)
+        <div className="flex flex-col items-end">
+          <span className="text-sm text-gray-900 dark:text-white">
+          Renato, a 12-year-old boy, is head over heels in love with Malena, a simple and beautiful woman. Meanwhile, things become worse for Malena when the townsfolk start spreading rumours about her.
           </span>
           <a
             href="/"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm mt-4 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Details
           </a>
