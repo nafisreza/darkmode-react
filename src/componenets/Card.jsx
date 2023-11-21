a7be0161b5e22c156166dac1827c531bb22f5bf4
nafisreza <bigganinafis@gmail.com>
2023-11-21 19:23:29 +0600

    Card Component markup changed

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -6,14 +6,14 @@ export default function Card() {
       <a href="/">
         <img
           className="p-8 rounded-t-lg"
-          src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="product_image1"
+          src="https://i.pinimg.com/originals/2e/53/f2/2e53f2afb0fdeb1290269d2a789eef9e.jpg"
+          alt="Monica Bellucci"
         />
       </a>
       <div className="px-5 pb-5">
         <a href="/">
           <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+            Monica Bellucci
           </h5>
         </a>
         <div className="flex items-center mt-2.5 mb-5">
@@ -68,13 +68,13 @@ export default function Card() {
         </div>
         <div className="flex items-center justify-between">
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            $599
+            Malena (2000)
           </span>
           <a
             href="/"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Add to cart
+            Details
           </a>
         </div>
       </div>
