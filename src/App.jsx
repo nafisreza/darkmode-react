4bd7de79eb2432f34507ba9fb87469fdaaed42b7
nafisreza <bigganinafis@gmail.com>
2023-11-21 19:59:19 +0600

    Dark mode using custom useTheme() hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ThemeBtn from "./componenets/ThemeBtn";
 import Card from "./componenets/Card";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const darkTheme = ()=> {
+    setThemeMode('dark')
+  }
+  const lightTheme = ()=> {
+    setThemeMode('light')
+  }
+
+  useEffect(() => {
+    const html = document.querySelector('html')
+    
+    html.classList.remove('dark', 'light')
+    html.classList.add(themeMode) 
+    
+  }, [themeMode])
+  
+
   return (
-    <>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -15,7 +34,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+      </ThemeProvider>
   );
 }
 
